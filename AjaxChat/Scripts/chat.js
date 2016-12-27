1ed6b3160beb8fda03c3389e78d113234b38c3b3
alex.paclin <alex.paclin@gmail.com>
2016-12-27 21:06:09 +0500

    some js for index.html

--- AjaxChat/Scripts/chat.js
+++ AjaxChat/Scripts/chat.js
@@ -1,1 +1,25 @@
-﻿
+﻿$(document).ready(function () {
+	// логин
+	$("#btnLogin").click(function () {
+		var nickName = $("#txtUserName").val();
+		if (nickName) {
+			// формируем ссылку с параметрами, по которой идет обращение
+			var href = "/Home?user=" + encodeURIComponent(nickName);
+			href = href + "&logOn=true";
+			$("#LoginButton").attr("href", href).click();
+
+			//установка поля с ником пользователя
+			$("#Username").text(nickName);
+		}
+	});
+});
+
+//при успешном входе загружаем сообщения
+function LoginOnSuccess(result) {
+	
+}
+
+//при ошибке отображаем сообщение об ошибке при логине
+function LoginOnFailure(result) {
+	
+}
